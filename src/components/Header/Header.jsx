3094b76041c86e86c927180623150bdcb2b01c15
Aleksndr <42521426+167899@users.noreply.github.com>
2023-10-01 21:15:17 +0300

    fix adaptive layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,29 @@
 import { NavItems } from 'components/NavItems/NavItems';
 import logo2 from './logo2.png';
 import css from './Header.module.css';
+import { SvgSelector } from 'components/Icons/Icons';
 
 export const Header = () => {
   return (
     <header className={css.header}>
       <div className={`container containerFlex ${css.containerHeaderFlex}`}>
-        <div>
+        <div className={css.iconContainer}>
           <img
             width="73px"
             height="82px"
             src={logo2}
             alt="logo"
-            className={css.logo2}
+            className={css.logo}
           />
+          <SvgSelector id={'boorger'}></SvgSelector>
         </div>
         <NavItems />
         <div className={css.phoneNumber}>
-        <a className={css.phoneNumberLink} href="tel:+380660245006">
-                  <span>тел. </span>
-                  +38 066 024 50 06
-                </a>
-          {/* <p>тел. 066 024 50 06</p> */}
+          <a className={css.phoneNumberLink} href="tel:+380660245006">
+            <span>тел. </span>
+            +38 066 024 50 06
+          </a>
         </div>
-        
       </div>
     </header>
   );
